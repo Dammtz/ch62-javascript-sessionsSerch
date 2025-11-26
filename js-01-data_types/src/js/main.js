393fadb0c58fc67818f8215dac4b4d5273f24175
Damara Martinez <damara.sulim.mtz@gmail.com>
2025-11-26 12:37:55 -0600

    feat: Cambio en JS practica tipo de datos

---
+++ js-01-data_types/src/js/main.js
@@ -0,0 +1,11 @@
+const userName = "Dam";
+const userLastName = "Martinez";
+let from = "Estado de mexico";
+let numHome = 47; 
+
+// ${} Interpolacion y \n hacer saltos de linea
+console.log (
+    `Hola, mi nombre es ${userName}, me apellido ${userLastName} \n y vivo en el ${from} donde mi numero de casa es ${numHome}`
+);
+
+console.log (typeof numHome); 
