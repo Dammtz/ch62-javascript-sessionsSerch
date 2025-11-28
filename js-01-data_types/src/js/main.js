841e57c2e86ab0e5c967a48cf8fb1d329b4ad38a
Damara Martinez <damara.sulim.mtz@gmail.com>
2025-11-27 23:54:40 -0600

    feat: combinacion de notas de tipo de datos

--- js-01-data_types/src/js/main.js
+++ js-01-data_types/src/js/main.js
@@ -1,12 +1,12 @@
 
-const userName = "Dam";
+const nombre = "Dam";
 const userLastName = "Martinez";
 let from = "Estado de mexico";
 let numHome = 47; 
 
 // ${} Interpolacion y \n hacer saltos de linea
 console.log (
-    `Hola, mi nombre es ${userName}, me apellido ${userLastName} \n y vivo en el ${from} donde mi numero de casa es ${numHome}`
+    `Hola, mi nombre es ${nombre}, me apellido ${userLastName} \n y vivo en el ${from} donde mi numero de casa es ${numHome}`
 );
 
 console.log (typeof numHome); 
