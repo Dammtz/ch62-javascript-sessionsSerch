97fe5fd5147fd7b87dbbd6fd48fa7ea145017331
Damara Martinez <damara.sulim.mtz@gmail.com>
2025-11-29 23:24:00 -0600

    Message Ultimas notas en Js sobre funciones

---
+++ js-03-control_flow/src/js/main.js
@@ -0,0 +1,61 @@
+/*
+ El control flow (flujo de control) en JavaScript describe cómo 
+ se ejecutan las instrucciones de un programa, desde el inicio 
+ hasta el final. Se basa en estructuras como secuencias, 
+ condiciones y bucles que determinan qué parte del código se ejecuta y cuándo.
+
+*/
+
+// ----------------- Condicional if-else --------------------
+/*
+ Ejecuta una sentencia si una condición específica es
+ evaluada como verdadera.
+
+ Sintaxis:
+    if ( condición ) sentencia;
+
+    if ( condición ) {
+        sentencias;
+    }
+
+    if ( condición ) sentencia;
+    else sentencia_si_condición_es_falsa;
+
+    if ( condición ) {
+        sentencias;
+    } else {
+        sentencias;
+    }
+
+    if (condición) sentencia;
+    else if (condicion 2) sentencia;
+         else sentencia_si_condicion2_es_falsa;
+
+
+    if ( condicion1 ) sentencias;
+    else if ( condicion2 ) sentencia;
+    else if ( condicion3 ) sentencia;
+    else if ( condicion4 ) sentencia;
+     .....
+    else if ( condicionN ) sentencia;
+    else condición;   
+
+*/
+
+/*Realizar funcion que reciba un numero como valor de temperatura,
+si la temperatura es mayor a 25 grados, retornar [hace calor], 
+en caso contrario, retornar hace frio;
+Imprimir el valor de retorno por consola.
+*/
+
+const clima = temperatura => {
+    let message = ("");
+    
+    if  (temperatura > 25) {message ("Hace calorcito");
+    } else {
+        message ("Hace frio");
+    }
+    return message; 
+};
+   console.log (clima (30));
+
