62f7978ba4ad14d0649014a48a080910662c51c4
Damara Martinez <damara.sulim.mtz@gmail.com>
2025-12-01 13:03:56 -0600

    Feat: Ejercicio Dam sobre arrays

---
+++ js-04-arrays_and_loops/src/js/main.js
@@ -0,0 +1,16 @@
+// --- 2. Acceso a Elementos ---
+// Los índices comienzan en 0 (Zero-based indexing).
+// [0] -> Primer elemento
+// [length - 1] -> Último elemento
+
+const techStack = ["HTML", "CSS", "JavaScript"];
+
+// Acceder al primer elemento
+console.log( techStack.length ); // 3
+console.log(`Accediendo al primer elemento:  ${ techStack[0] }` ); // "HTML"
+
+// Accediendo al segundo elemento
+console.log (`Accediendo al segundo elemento:  ${ techStack[1] }`); // "CSS"
+
+// Acceder al último elemento (longitud - 1)
+console.log (`Accediendo al ultimo elemento:  ${ techStack[length-1] }` ); // "JavaScipt"
