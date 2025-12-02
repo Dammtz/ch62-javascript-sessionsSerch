3798768df79c54324798db81f89d21d0d91a4e76
Damara Martinez <damara.sulim.mtz@gmail.com>
2025-12-02 14:00:13 -0600

    Feat: Problemas por resolver Dam

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -79,3 +79,54 @@ console.log("Lista actual:", shoppingList);
 // 4. shift(): Elimina el PRIMERO y lo devuelve
 const firstRemoved = shoppingList.shift();
 console.log(`Eliminado con shift: ${firstRemoved}`); // "Café"
+
+// TODO EJERCICIO RÁPIDO: Dam 
+// 1. Crea un array 'guestList' con 2 nombres.
+// 2. Agrega un invitado al final.
+// 3. Agrega un invitado al principio
+// 4. Elimina al segundo invitado (índice 1) y agrega a otro en su lugar.
+// 4. Muestra la lista final.
+
+/*const guestList = ("Lizet", "Dam");
+   guestList.push ("Emma");
+   guestList.unshift ("Miguel");
+   guestList.splice (1, 1, 'Mario'); 
+   //console.log ( 'Lista final de nuestros invitados:', guestList.join ("-")); 
+
+
+ Ejercicio:
+   Crea un array llamado 'taskList' para gestionar tareas.
+   1. Agrega 3 tareas usando 'push'.
+   2. Muestra la lista completa de tareas.
+   3. Las tareas normales se atienden en orden de llegada (FIFO)
+   3.1 Retira (elimina) la primera tarea de la lista y muéstrala.
+   4. No dan un tarea URGENTE que debe ser atendido inmediatamente.
+    Por lo que debes poner al inicio de la lista.
+   5. Muestra la lista actualizada.
+   6. Atiende (elimina) la primera tarea de la lista y muéstrala.
+
+ const taskList = []; 
+  taskList.push ("Tender cama", "Lavar trastes", "Ordenar cuarto"); // Agregar 3 tareas 
+  console.log ("Lista de tareas por hacer:" + taskList.join (" -"));  // Mostrar lista completa 
+
+  const fisrtTask = tasklist.shift(); // Retirar la primera tarea de la lista 
+   console.log ('Tarea hecha', fisrtTask);
+   taskList.unshift ("Pagar la luz"); // Agregar tarea urgente al inicio de la lista 
+   console.log ("Lista actualizada de mis tareas", tasklist.join ("-")); // Mostrar lista actualizada 
+
+   const secondTask = taskList.shift(); // Atendindo la primera tarea de la lista 
+   console.log ("Tarea hecha:", secondTask); 
+   */ 
+
+   // TODO: RETO FINAL (Simulación de Cajero)
+// Tienes un array de movimientos: [100.00, -50.00, 200.00, -100.00]
+// 1. Usa un ciclo FOR, o WHILE o FOR-OF para recorrer los movimientos.
+// 2. Suma los valores a una variable 'totalBalance'.
+// 3. Imprime el balance final.
+const transactions = [100.00, -50.00, 200.00, -100.00, 500.00]; //Array movimientos 
+let totalBalance = 0;  // Variable para el balance total 
+
+for (const monto of transactions) {
+       totalBalance += monto; // sumar valores a una variable 
+       console.log ('Movimiento:', monto, 'Balance actual:', totalBalance);
+}
